eb169c639e31836f35db5a960970f37c1372b24c
Aidar Zinnatullin <aidar.zinnatulla@gmail.com>
2024-08-26 09:53:47 +0200

    prep for NER

---
+++ prep_for_NER.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+load("covid_tonline.RData")
+nrow(covid_tonline)
+head(covid_tonline)
+
+30166/4
+covid_tonline_1 <- covid_tonline[c(1:5000),]
+covid_tonline_1$lemmatized_text <- NULL
+write_csv(covid_tonline_1, file = "csv_for_ner_vienna_1.csv")
+
+
+covid_tonline_2 <- covid_tonline[c(5001:6000),]
+covid_tonline_2$lemmatized_text <- NULL
+write_csv(covid_tonline_2, file = "csv_for_ner_vienna_2.csv")
+
+
+covid_tonline_3 <- covid_tonline[c(6001:7000),]
+covid_tonline_3$lemmatized_text <- NULL
+write_csv(covid_tonline_3, file = "csv_for_ner_vienna_3.csv")
+
+covid_tonline_4 <- covid_tonline[c(7001:7400),]
+covid_tonline_4$lemmatized_text <- NULL
+write_csv(covid_tonline_4, file = "csv_for_ner_vienna_4.csv")
+
+covid_tonline_rest <- covid_tonline[c(7401:30166),]
+
+
+# for HPC -----------------------------------------------------------------
+
+
+names(covid_tonline_copy)
+covid_tonline_rest$lemmatized_text <- NULL
+
+write_csv(covid_tonline_rest, file = "csv_for_ner_vienna_copy_all.csv")
+
